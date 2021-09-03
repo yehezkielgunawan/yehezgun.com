21553db83543fa6507f576d0b8052dc5b76aa627
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-09-03 11:44:06 +0700

    feat(settings): :sparkles: add animation when the page loads using framer-motion
    Maybe it's still seems redundant, will improve it later

--- src/pages/aboutme/index.tsx
+++ src/pages/aboutme/index.tsx
@@ -2,7 +2,6 @@ import {
   Divider,
   Flex,
   Heading,
-  HStack,
   Icon,
   Image,
   Link as ChakraLink,
@@ -12,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { GoPrimitiveDot } from "react-icons/go";
+import MotionStack from "../../components/motion/MotionStack";
 import AppHeader from "../../components/ui/AppHeader";
 import { useAppToast } from "../../components/ui/AppToast";
 import { Main } from "../../components/wrapper/Main";
@@ -47,79 +47,88 @@ function AboutMe({ experienceList }: { experienceList: Experiences }) {
   }, []);
 
   return (
-    <Main>
-      <AppHeader
-        pageTitle="About Me"
-        pageDesc="A little description about me."
-        route="aboutme"
-      />
-      <Flex
-        justifyContent="space-between"
-        gridGap={isDesktopWidth ? 8 : 4}
-        align="center"
-      >
-        <Stack spacing={isDesktopWidth ? 4 : 3}>
-          <Heading as="h5">Yo, hi there!</Heading>
-          {!isDesktopWidth && (
-            <Image src="/assets/yehez-profile.png" objectFit="contain" />
+    <MotionStack
+      variants={{
+        before: { opacity: 0, y: 20, transition: { type: "spring" } },
+        after: { opacity: 1, y: 0, transition: { type: "spring" } },
+      }}
+      initial="before"
+      animate="after"
+    >
+      <Main>
+        <AppHeader
+          pageTitle="About Me"
+          pageDesc="A little description about me."
+          route="aboutme"
+        />
+        <Flex
+          justifyContent="space-between"
+          gridGap={isDesktopWidth ? 8 : 4}
+          align="center"
+        >
+          <Stack spacing={isDesktopWidth ? 4 : 3}>
+            <Heading as="h5">Yo, hi there!</Heading>
+            {!isDesktopWidth && (
+              <Image src="/assets/yehez-profile.png" objectFit="contain" />
+            )}
+            <Text textAlign="justify">
+              I’m Yehezkiel Gunawan, a frontend engineer. You can call me Yehez.
+              Currently, I like to craft some web apps with React and
+              Typescript. I make fun projects or write some articles in my free
+              time and publish it here.
+              <br /> <br />I like to explore some new tech stuff, playing games
+              sometimes, and watching animes.
+            </Text>
+            <Flex gridGap={3} wrap="wrap">
+              {contactList.map((contact, index) => (
+                <ChakraLink key={index} isExternal href={contact.link_route}>
+                  <Icon
+                    _hover={{
+                      bgColor: "gray.500",
+                    }}
+                    as={contact.icon}
+                    fontSize="4xl"
+                  />
+                </ChakraLink>
+              ))}
+            </Flex>
+          </Stack>
+          {isDesktopWidth && (
+            <Image
+              src="/assets/yehez-profile.png"
+              objectFit="contain"
+              w="40%"
+              borderRadius={4}
+            />
           )}
-          <Text textAlign="justify">
-            I’m Yehezkiel Gunawan, a frontend engineer. You can call me
-            Yehez. Currently, I like to craft some web apps with React and
-            Typescript. I make fun projects or write some articles in my free
-            time and publish it here.
-            <br /> <br />I like to explore some new tech stuff, playing games
-            sometimes, and watching animes.
-          </Text>
-          <HStack spacing={3}>
-            {contactList.map((contact, index) => (
-              <ChakraLink key={index} isExternal href={contact.link_route}>
-                <Icon
-                  _hover={{
-                    bgColor: "gray.500",
-                  }}
-                  as={contact.icon}
-                  fontSize="4xl"
-                />
-              </ChakraLink>
-            ))}
-          </HStack>
+        </Flex>
+        <Divider borderWidth={2} />
+        <Stack spacing={4} py={4}>
+          <Heading as="h5">Work Experiences</Heading>
+          {dataExperiences.map((experience, index) => {
+            return (
+              <Skeleton key={index} isLoaded={experience ? true : false}>
+                <Flex gridGap={3} align="center">
+                  <GoPrimitiveDot />
+                  <Stack spacing={2}>
+                    <Text fontSize="md">
+                      <b>{experience.role_name}</b>
+                    </Text>
+                    <Text fontSize="sm">{experience.company_name}</Text>
+                    <Text fontSize="md">
+                      <b>
+                        <i>{experience.duration}</i>
+                      </b>
+                    </Text>
+                  </Stack>
+                </Flex>
+                <Divider />
+              </Skeleton>
+            );
+          })}
         </Stack>
-        {isDesktopWidth && (
-          <Image
-            src="/assets/yehez-profile.png"
-            objectFit="contain"
-            w="40%"
-            borderRadius={4}
-          />
-        )}
-      </Flex>
-      <Divider borderWidth={2} />
-      <Stack spacing={4} py={4}>
-        <Heading as="h5">Work Experiences</Heading>
-        {dataExperiences.map((experience, index) => {
-          return (
-            <Skeleton key={index} isLoaded={experience ? true : false}>
-              <Flex gridGap={3} align="center">
-                <GoPrimitiveDot />
-                <Stack spacing={2}>
-                  <Text fontSize="md">
-                    <b>{experience.role_name}</b>
-                  </Text>
-                  <Text fontSize="sm">{experience.company_name}</Text>
-                  <Text fontSize="md">
-                    <b>
-                      <i>{experience.duration}</i>
-                    </b>
-                  </Text>
-                </Stack>
-              </Flex>
-              <Divider />
-            </Skeleton>
-          );
-        })}
-      </Stack>
-    </Main>
+      </Main>
+    </MotionStack>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,6 +32,7 @@ import { Projects } from "../functions/lib/types";
 import { techStackList } from "../constants/techStacks";
 import { useAppToast } from "../components/ui/AppToast";
 import AppHeader from "../components/ui/AppHeader";
+import MotionStack from "../components/motion/MotionStack";
 
 export async function getStaticProps() {
   const projectList = await getAllProjects();
@@ -60,107 +61,116 @@ const Index = ({ newestProjects }: { newestProjects: Projects }) => {
   }, [dataProjects]);
 
   return (
-    <Main>
-      <AppHeader
-        pageTitle="Home"
-        pageDesc="Welcome to my personal site. Take a look and enjoy."
-      />
-      <Flex justifyContent="space-between" align="center" gridGap={4}>
-        <Stack spacing={2}>
-          <Text fontSize="2xl">
-            <b>Hi, I'm Yehezkiel Gunawan.</b>
+    <MotionStack
+      variants={{
+        before: { opacity: 0, y: 20, transition: { type: "spring" } },
+        after: { opacity: 1, y: 0, transition: { type: "spring" } },
+      }}
+      initial="before"
+      animate="after"
+    >
+      <Main>
+        <AppHeader
+          pageTitle="Home"
+          pageDesc="Welcome to my personal site. Take a look and enjoy."
+        />
+        <Flex justifyContent="space-between" align="center" gridGap={4}>
+          <Stack spacing={2}>
+            <Text fontSize="2xl">
+              <b>Hi, I'm Yehezkiel Gunawan.</b>
+            </Text>
+
+            <Text fontSize="md" textAlign="justify">
+              Currently work as a frontend developer.
+              <br />
+              You've found my personal slice of the internet. Take a look and
+              enjoy.
+            </Text>
+          </Stack>
+          {isDesktopWidth && (
+            <Image
+              src="https://yt3.ggpht.com/a/AGF-l78ZZR7LqHV1HwgAG8xhGoQt-CTeEVS0rmcG=s900-c-k-c0xffffffff-no-rj-mo"
+              boxSize="12rem"
+              objectFit="contain"
+            />
+          )}
+        </Flex>
+
+        <Stack pt={12} spacing={2}>
+          <Text>
+            <b>Current Favourite Tech Stacks</b>
           </Text>
-
-          <Text fontSize="md" textAlign="justify">
-            Currently work as a frontend developer.
-            <br />
-            You've found my personal slice of the internet. Take a look and
-            enjoy.
-          </Text>
-        </Stack>
-        {isDesktopWidth && (
-          <Image
-            src="https://yt3.ggpht.com/a/AGF-l78ZZR7LqHV1HwgAG8xhGoQt-CTeEVS0rmcG=s900-c-k-c0xffffffff-no-rj-mo"
-            boxSize="12rem"
-            objectFit="contain"
-          />
-        )}
-      </Flex>
-
-      <Stack pt={12} spacing={2}>
-        <Text>
-          <b>Current Favourite Tech Stacks</b>
-        </Text>
-        <HStack spacing={4}>
-          {techStackList.map((techStack, index) => (
-            <Popover key={index} placement="top" trigger="hover">
-              <PopoverTrigger>
-                <Box as="button">
-                  <Icon
-                    _hover={{
-                      color: "gray.500",
-                    }}
-                    as={techStack.icon}
-                    fontSize="4xl"
-                  />
-                </Box>
-              </PopoverTrigger>
-              <PopoverContent>
-                <PopoverArrow />
-                <PopoverCloseButton />
-                <PopoverBody>{techStack.description}</PopoverBody>
-              </PopoverContent>
-            </Popover>
-          ))}
-        </HStack>
-      </Stack>
-
-      <Stack spacing={3} pt={12}>
-        <Text fontSize="lg">
-          <b>Newest Projects</b>
-        </Text>
-        <SimpleGrid columns={[1, null, 2]} spacing={3}>
-          {dataProjects.map((project, index) => {
-            return (
-              <Skeleton key={index} isLoaded={dataProjects ? true : false}>
-                <ChakraLink isExternal href={project.project_url}>
-                  <Box
-                    p={1}
-                    overflow="hidden"
-                    borderRadius={10}
-                    borderWidth={2}
-                    _hover={{
-                      bg: "gray.500",
-                    }}
-                  >
-                    <Stack spacing={3} align="center">
-                      <Text textAlign="center" fontSize="lg">
-                        <b>{project.project_title}</b>
-                        <ExternalLinkIcon pl={1} />
-                      </Text>
-                      <Divider></Divider>
-                      <Img
-                        src={project.image_url[0].url}
-                        objectFit="contain"
-                        w="100%"
-                        h="150px"
-                        align="center"
-                      />
-                    </Stack>
+          <HStack spacing={4}>
+            {techStackList.map((techStack, index) => (
+              <Popover key={index} placement="top" trigger="hover">
+                <PopoverTrigger>
+                  <Box as="button">
+                    <Icon
+                      _hover={{
+                        color: "gray.500",
+                      }}
+                      as={techStack.icon}
+                      fontSize="4xl"
+                    />
                   </Box>
-                </ChakraLink>
-              </Skeleton>
-            );
-          })}
-        </SimpleGrid>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverBody>{techStack.description}</PopoverBody>
+                </PopoverContent>
+              </Popover>
+            ))}
+          </HStack>
+        </Stack>
 
-        <NextLink href="/projects" passHref>
-          <Button as="a" variant="outline" width="">
-            See More Projects
-          </Button>
-        </NextLink>
-      </Stack>
-    </Main>
+        <Stack spacing={3} pt={12}>
+          <Text fontSize="lg">
+            <b>Newest Projects</b>
+          </Text>
+          <SimpleGrid columns={[1, null, 2]} spacing={3}>
+            {dataProjects.map((project, index) => {
+              return (
+                <Skeleton key={index} isLoaded={dataProjects ? true : false}>
+                  <ChakraLink isExternal href={project.project_url}>
+                    <Box
+                      p={1}
+                      overflow="hidden"
+                      borderRadius={10}
+                      borderWidth={2}
+                      _hover={{
+                        bg: "gray.500",
+                      }}
+                    >
+                      <Stack spacing={3} align="center">
+                        <Text textAlign="center" fontSize="lg">
+                          <b>{project.project_title}</b>
+                          <ExternalLinkIcon pl={1} />
+                        </Text>
+                        <Divider></Divider>
+                        <Img
+                          src={project.image_url[0].url}
+                          objectFit="contain"
+                          w="100%"
+                          h="150px"
+                          align="center"
+                        />
+                      </Stack>
+                    </Box>
+                  </ChakraLink>
+                </Skeleton>
+              );
+            })}
+          </SimpleGrid>
+
+          <NextLink href="/projects" passHref>
+            <Button as="a" variant="outline" width="">
+              See More Projects
+            </Button>
+          </NextLink>
+        </Stack>
+      </Main>
+    </MotionStack>
   );
 };
 
