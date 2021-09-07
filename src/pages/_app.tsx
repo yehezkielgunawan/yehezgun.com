7273d95faf662773efd3f3e7235f2646df222db7
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-09-07 14:57:27 +0700

    style: :art: add default SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,19 @@ import { AppProps } from "next/app";
 import { Container } from "../components/wrapper/Container";
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://yehezgun.com/",
+          site_name: "YehezGun",
+        }}
+      />
       <Container>
         <Component {...pageProps} />
       </Container>
