d0b1d9ea5e04714aec40b116f3dd34a8fa4aff20
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-09-07 17:37:15 +0700

    style: :art: change font size on meta image

--- src/pages/articles/post/[slug].tsx
+++ src/pages/articles/post/[slug].tsx
@@ -74,7 +74,7 @@ const BlogPost: React.FC<{ post: SingleArticle; blocks: BlockMapType }> = ({
               {
                 url: `https://yehez-og-image.yehezgun.com/**${encodeURIComponent(
                   post.title.trim(),
-                )}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg`,
+                )}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=50px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=250&heights=250`,
                 alt: `${post.title} | YehezGun`,
                 width: 800,
                 height: 600,
