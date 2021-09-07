a4afe85e94ef65300a60cbe1471fcb9a133dab52
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-09-07 11:37:37 +0700

    style: :art: change og image icon for the core page

--- src/components/ui/AppHeader.tsx
+++ src/components/ui/AppHeader.tsx
@@ -20,7 +20,7 @@ const AppHeader = ({ pageTitle, pageDesc, route }: AppHeaderProps) => {
       <meta property="og:title" content={`${pageTitle} | YehezGun`} />
       <meta
         property="og:image"
-        content={`https://yehez-og-image.yehezgun.com/**${pageTitle}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=250&heights=auto`}
+        content={`https://yehez-og-image.yehezgun.com/**${pageTitle}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630987481%2FYG_logo_trg6fp.png&widths=200`}
       />
       <meta property="og:description" content={pageDesc} />
 
@@ -29,7 +29,7 @@ const AppHeader = ({ pageTitle, pageDesc, route }: AppHeaderProps) => {
       <meta name="twitter:description" content={pageDesc} />
       <meta
         name="twitter:image"
-        content={`https://yehez-og-image.yehezgun.com/**${pageTitle}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=250&heights=auto`}
+        content={`https://yehez-og-image.yehezgun.com/**${pageTitle}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630987481%2FYG_logo_trg6fp.png&widths=200`}
       />
     </Head>
   );
