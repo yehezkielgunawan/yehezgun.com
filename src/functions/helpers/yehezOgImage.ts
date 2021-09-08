4f2a4949672de221e634ae94834ebf8065c92a9f
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-09-08 11:40:29 +0700

    style(settings): :art: modularized again yehezOGImage

--- src/functions/helpers/yehezOgImage.ts
+++ src/functions/helpers/yehezOgImage.ts
@@ -1,5 +1,5 @@
 export const yehezOgImage = (text: string, isArticle: boolean) => {
-  if (isArticle === true) {
+  if (isArticle) {
     return `https://yehez-og-image.yehezgun.com/**${encodeURIComponent(
       text.trim(),
     )}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=50px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=250&heights=250`;
