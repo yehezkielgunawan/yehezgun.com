6de596c650a45e29ec888821dded77733bb9d448
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2021-10-04 17:47:57 +0700

    style(yehezOgImage): :art: change article image layout

--- src/functions/helpers/yehezOgImage.ts
+++ src/functions/helpers/yehezOgImage.ts
@@ -1,8 +1,8 @@
-export const yehezOgImage = (text: string, isArticle: boolean): string  => {
+export const yehezOgImage = (text: string, isArticle: boolean): string => {
   if (isArticle) {
     return `https://yehez-og-image.yehezgun.com/**${encodeURIComponent(
       text.trim(),
-    )}**%20%7C%20YehezGun.png?theme=dark&md=1&fontSize=50px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=250&heights=250`;
+    )}**%3Cbr%3Eby%20YehezGun.png?theme=dark&md=1&fontSize=50px&images=https%3A%2F%2Fres.cloudinary.com%2Fyehez%2Fimage%2Fupload%2Fv1630902976%2FAssassination_Classroom_-_Koro-sensei_smiling_head_fwpndi.svg&widths=250&heights=250`;
   }
   return `https://yehez-og-image.yehezgun.com/**${encodeURIComponent(
     text.trim(),
